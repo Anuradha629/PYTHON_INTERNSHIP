9caca89a6541348e1b6b55b14e6e6b179bef5af2
Anuradha629 <anuradhakarhale629@gmail.com>
2026-07-24 15:09:34 +0530

    Updated Databse file

--- db.py
+++ db.py
@@ -1,12 +1,123 @@
 import sqlite3
 
-DB = "coaching.db"
+DATABASE = "coaching.db"
+
+
+# ================= DATABASE CONNECTION =================
 
 def get_connection():
-    conn = sqlite3.connect(DB)
+
+    conn = sqlite3.connect(DATABASE)
+
     conn.row_factory = sqlite3.Row
+
     return conn
 
-def close_connection(conn):
-    if conn:
-        conn.close()
+
+
+# ================= DATABASE CREATION =================
+
+def init_db():
+
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+
+    # Users Table
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        password TEXT
+
+    )
+    """)
+
+    cursor.execute("""
+    SELECT * FROM users
+    WHERE username='Anuradha'
+    """)
+
+    user = cursor.fetchone()
+
+    if not user:
+        cursor.execute("""
+        INSERT INTO users(username,password)
+        VALUES(?,?)
+        """,
+        ("Anuradha","anuradha123"))
+
+
+        # Students Table
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS students(
+
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        roll_no TEXT UNIQUE,
+        name TEXT,
+        father_name TEXT,
+        surname TEXT,
+        mobile TEXT,
+        course TEXT,
+        batch TEXT,
+        address TEXT,
+        admission_date TEXT,
+        status TEXT
+
+    )
+    """)
+
+
+
+    # Teachers Table
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS teachers(
+
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        subject TEXT,
+        mobile TEXT,
+        timing TEXT,
+        experience TEXT
+
+    )
+    """)
+
+
+
+    # Fees Table
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS fees(
+
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        student_id INTEGER,
+        total_fee INTEGER,
+        paid_fee INTEGER,
+        date TEXT
+
+    )
+    """)
+
+
+    # Attendance Table
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS attendance(
+
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        student_id INTEGER,
+        date TEXT,
+        status TEXT
+
+    )
+    """)
+
+
+    conn.commit()
+    conn.close()
